d1db79f1d271153ff5cb219d133388e5461a3d88
yosuke0517 <x.clover.x007@gmail.com>
2019-05-15 22:45:19 +0900

    .ideaをgitignoreへ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(name: 'sample',)
 
 20.times do |number|
   Question.create(id:"#{number+1}", name: 'Test name' + "#{number+1}",title: 'Test title'  + "#{number+1}",
