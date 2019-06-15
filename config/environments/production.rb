bfcd5895b8fa8bc6ff95b0f86219574ed83de2fa
yosuke0517 <x.clover.x007@gmail.com>
2019-06-15 18:05:43 +0900

    active_storage.serviceが"local"になっていたのを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -93,6 +94,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #activestorage
-  config.active_storage.service = :amazon
 end
